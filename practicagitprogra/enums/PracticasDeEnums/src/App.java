cbd197d0cd9cef35266ecf0d56f390536b75a08f
ger12xela <ger12xela@gmail.com>
2021-07-23 12:22:25 -0600

    se creo la clase figuras geometricas

--- practicagitprogra/enums/PracticasDeEnums/src/App.java
+++ practicagitprogra/enums/PracticasDeEnums/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("1, AMARILLO");
+        System.out.println("2, AZUL");
+        System.out.println("3, ROJO");
+        
+        int opcion = 1; //usuario lo ingreso
+
+        FiguraGeometrica figura = new FiguraGeometrica();
+        
     }
 }
---
+++ practicagitprogra/enums/PracticasDeEnums/src/FiguraGeometrica.java
@@ -0,0 +1,22 @@
+package practicagit.practicagitprogra.enums.PracticasDeEnums.src;
+
+public class FiguraGeometrica {
+
+    public static final int AMARILLO = 1;
+    public static final int AZUL = 2;
+    public static final int ROJO = 3;
+
+    private int color;
+
+    public FiguraGeometrica (int color){
+        this.color = color;
+    }
+
+    public int getColor() {
+        return color;
+
+    }
+    public void setColor(int color){
+        this.color = color;
+    }
+}
